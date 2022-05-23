96324e5548fae257a577a747980075b8921af33a
Roy263 <souravroy.sj@gmail.com>
2022-05-23 17:07:04 +0530

    partial implementation

--- Mountain Array/MountainArr.py
+++ Mountain Array/MountainArr.py
@@ -1,8 +1,25 @@
-def findmax(arr):
+def findmaxLinerarSearch(array):
     max=0
     for i in arr:
         if i>max:
             max=i
     return max
-arr=[7,8,13,15,17,12,9,5,2]
-print(findmax(arr))
+
+def findMaxBinarySearch(arr):
+    n=len(arr)
+    mid=int(n/2)
+    if(n==2): 
+        return arr[0] if arr[0]>arr[1] else arr[1]
+    else:
+        m=arr[mid]
+        if(m>arr[mid-1] and m<arr[mid+1]):
+            print("mid is peak")
+            return m
+        if(m>arr[mid-1] and arr[mid+1]>m):
+            print("peak is on the right")
+            return findmaxLinerarSearch(arr[mid:n-1])
+        if(m<arr[mid-1] and m>arr[mid+1]):
+            print("peak is on the left")
+            return findmaxLinerarSearch(arr[:mid])
+arr=[7,8,11,12,13,15,9,5,2]
+print(findMaxBinarySearch(arr))
