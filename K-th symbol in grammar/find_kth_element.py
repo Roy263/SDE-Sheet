13edb79cf4ada378cc799b6f8556a64e70302010
Roy263 <souravroy.sj@gmail.com>
2023-09-19 15:48:02 +0530

    modified with recursion

--- K-th symbol in grammar/find_kth_element.py
+++ K-th symbol in grammar/find_kth_element.py
@@ -6,29 +6,39 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        flag=1
-        k-=1
-        print("k:",k)
+        kth=0
+        if(n==1 and k==1):
+            return kth
         rowsize=2**(n-1)
-        print(rowsize)
-        left=0
-        right=rowsize-1
-        while left<right:
-            mid=left+(right-left)//2
-            print(f"left:{left} right:{right} mid:{mid}")
-            time.sleep(2)
+        mid=rowsize//2
+        if(k<=mid):
+            result=self.kthGrammar(n-1,k)
+        else:
+            result=int(not self.kthGrammar(n-1,k-mid))
+        return result
+        # flag=1
+        # k-=1
+        # print("k:",k)
+        # rowsize=2**(n-1)
+        # print(rowsize)
+        # left=0
+        # right=rowsize-1
+        # while left<right:
+        #     mid=left+(right-left)//2
+        #     print(f"left:{left} right:{right} mid:{mid}")
+        #     time.sleep(2)
             
-            if(k>mid):
-                flag=0
-                left=mid+1
-            elif (k<=mid):
-                right=mid
-        return (flag+1)%2  
+        #     if(k>mid):
+        #         flag=0
+        #         left=mid+1
+        #     elif (k<=mid):
+        #         right=mid
+        # return (flag+1)%2  
                 
 
 if __name__=="__main__":
     obj=Solution()
     n=4
-    k=6
+    k=3
     result=obj.kthGrammar(n,k)
     print(result)
